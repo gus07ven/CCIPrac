18f9de48f4514700d709cbd82f45080389995595
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-10-24 18:56:18 -0500

    Add insert between nodes for doubly linked list

--- DLinkedList.py
+++ DLinkedList.py
@@ -17,6 +17,7 @@ class DoublyLinkedList:
         while current_node is not None:
             print(current_node.data)
             current_node = current_node.next
+        print("")
 
     def insert_at_beginning(self, data):
         if self.head is None:
@@ -24,16 +25,44 @@ class DoublyLinkedList:
             return
         current_node = self.head
         new_node = Node(data)
-        current_node.next = new_node
-        new_node.prev = current_node
+        current_node.prev = new_node
+        new_node.next = current_node
+        self.head = new_node
+
+    def insert_between(self, first_node_data, second_node_data, data):
+        if first_node_data is None or second_node_data is None or data is None:
+            print("None of the values can be null. Please use valid values.")
+            return
+        current_node = self.head
+        first_node = None
+        while current_node is not None:
+            if current_node.data == first_node_data and current_node.next.data == second_node_data:
+                first_node = current_node
+                break
+            current_node = current_node.next
+        node_to_insert = Node(data)
+        second_node = first_node.next
+        node_to_insert.prev = first_node
+        node_to_insert.next = second_node
+        first_node.next = node_to_insert
+        second_node.prev = node_to_insert
 
 
 if __name__ == "__main__":
 
-    # Create doubly linked list and insert node at beginning
+    # Create doubly linked list skipping Wednesday, and Saturday
+    print("Creating days of the week without Wednesday and Saturday:")
     doubly_linked_list = DoublyLinkedList()
-    doubly_linked_list.insert_at_beginning("Monday")
+    doubly_linked_list.insert_at_beginning("Friday")
+    doubly_linked_list.insert_at_beginning("Thursday")
     doubly_linked_list.insert_at_beginning("Tuesday")
+    doubly_linked_list.insert_at_beginning("Monday")
+    doubly_linked_list.insert_at_beginning("Sunday")
+    doubly_linked_list.display()
+
+    # Insert Wednesday between Tuesday and Thursday
+    print("Inserting Wednesday between Tuesday and Thursday:")
+    doubly_linked_list.insert_between("Tuesday", "Thursday", "Wednesday")
     doubly_linked_list.display()
 
 
