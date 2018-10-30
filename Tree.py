079d3046740f62959cb1f8c55d10f947b69554fb
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-10-30 00:13:41 -0500

    Add binary tree

---
+++ Tree.py
@@ -0,0 +1,16 @@
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+class Tree:
+
+    def __init__(self, root):
+        self.root = root
+
+
+
