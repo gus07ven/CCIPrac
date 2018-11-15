5eb797abc70263f42806cd2be52f68a8b0cd099f
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-11-15 16:06:06 -0600

    Add and update keys in chained map

--- ChainedMap.py
+++ ChainedMap.py
@@ -13,11 +13,25 @@ print('Values = {}'.format((list(res.values()))))
 print()
 
 # Print all the elements from the result
-print("Elements:")
+print("Printing all elements:")
 for key, val in res.items():
     print('{} = {}'.format(key, val))
 print()
 
 # Find a specific value in the result
+print("Find specific values in dictionary:")
 print('day3 in res: {}'.format(('day1' in res)))
 print('day4 in res: {}'.format(('day4' in res)))
+print()
+
+# Adding elements to chained map
+print("Adding day4 = Fri to dictionary")
+dict_two['day4'] = 'Fri'
+print(res.maps, '\n')
+print()
+
+# Updating element in chained map
+print("Updating day2 = Sat in dictionary")
+dict_one['day2'] = 'Sat'
+print(res.maps, '\n')
+print()
