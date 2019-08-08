5cb4e28bf08f512f4997a7a555f42460efa06069
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-08 17:32:36 -0500

    Add another solution for 709

--- Leet709.py
+++ Leet709.py
@@ -1,6 +1,18 @@
 
-def toLowerCase(s):
-    return s.lower()
+class Leet709:
+
+    def to_lower_case(self, s):
+        return s.lower()
+
+    def to_lower_case_imp(self, s):
+        if len(s) == 0:
+            return ""
+
+        return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in s)
 
 
-print(toLowerCase("HELLO"))
+if __name__ == "__main__":
+    runner = Leet709()
+    input = "HELLO"
+    print(runner.to_lower_case(input))
+    print(runner.to_lower_case_imp(input))
