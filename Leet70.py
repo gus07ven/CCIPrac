4a252546a6e5dd8e46c3d514c7cbfd2101303baf
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-30 11:04:51 -0500

    Add top dowm memoization solution for Leet70

--- Leet70.py
+++ Leet70.py
@@ -4,14 +4,20 @@ from typing import List
 class Leet70:
 
     @staticmethod
-    def climb_stairs(self, n: int) -> int:
-        stepsTable = [0] * (n + 1)
-        return climb_stairs_aux(stepsTable, n)
+    def climb_stairs(n: int) -> int:
+        steps_table = [0] * (n + 1)
+        return Leet70.climb_stairs_aux(steps_table, n)
 
     @staticmethod
-    def climb_stairs_aux(self, stepsTable: List[int], n: int) -> int:
-        pass
+    def climb_stairs_aux(steps_table: List[int], n: int) -> int:
+        if n == 0 or n == 1 or n == 2:
+            return n
+        if steps_table[n] == 0:
+            steps_table[n] = Leet70.climb_stairs_aux(steps_table, n - 2) + Leet70.climb_stairs_aux(steps_table, n - 1)
+        return steps_table[n]
 
 
 if __name__ == "__main__":
-    print(Leet70.climb_stairs(5))
+    num_steps = 5
+    print("The number of distinct ways to climb to the top of a {0:d} step staircase is: {1:d}"
+          .format(num_steps, Leet70.climb_stairs(5)))
