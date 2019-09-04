cd6e646bbfa01abe603f77c6bc002b39c52c36ca
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-04 17:00:29 -0500

    Add 303 constructor

--- Leet303.py
+++ Leet303.py
@@ -1,4 +1,15 @@
+from typing import List
+
+
 class Leet303:
+
+    def __init__(self, nums: List):
+        self.numsDic = {}
+        for i in range(0, len(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
+                sum += nums[j]
+                self.numsDic[(i, j)] = sum
 
     def sum_range(self, i: int, j: int) -> int:
         return 0
@@ -6,3 +17,4 @@ class Leet303:
 
 if __name__ == "__main__":
     nums = [-2, 0, 3, -5, 2, -1]
+    le = Leet303(nums)
