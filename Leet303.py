3cb5c52b75afa0c51766d5ce10562cab725229f3
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-04 17:01:50 -0500

    Add sum_range leet 303

--- Leet303.py
+++ Leet303.py
@@ -12,9 +12,12 @@ class Leet303:
                 self.numsDic[(i, j)] = sum
 
     def sum_range(self, i: int, j: int) -> int:
-        return 0
+        return self.numsDic[(i, j)]
 
 
 if __name__ == "__main__":
     nums = [-2, 0, 3, -5, 2, -1]
     le = Leet303(nums)
+    print(le.sum_range(0, 2))
+    print(le.sum_range(2, 5))
+    print(le.sum_range(0, 5))
