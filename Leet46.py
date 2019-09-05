28a4f61c954a963276ac8cecf47f1fb70bee37fd
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-05 16:35:39 -0500

    Add solution for Leet 46

--- Leet46.py
+++ Leet46.py
@@ -1,15 +1,31 @@
 from typing import List
 
+
 class Leet46:
 
     def permute(self, nums: List) -> List[List]:
         results = []
-        return self.permute_rec(self, results, set(), nums)
+        self.backtrack(results, [], set(), nums)
+        return results
 
-    def permute_rec(self, results: List[List], ) -> List[List]:
-        return []
+    def backtrack(self, results: List[List], soFar: List, soFarSet: set, nums: List) -> None:
+        if len(soFar) == len(nums):
+            add = soFar.copy()
+            results.append(add)
+        else:
+            for i in nums:
+                if i in soFarSet:
+                    continue
+                soFarSet.add(i)
+                soFar.append(i)
+                self.backtrack(results, soFar, soFarSet, nums)
+                soFarSet.remove(i)
+                soFar.remove(i)
 
 
 if __name__ == "__main__":
     nums = [1, 2, 3]
-    result = permute(nums)
+    le = Leet46()
+    result = le.permute(nums)
+    for i in result:
+        print(i)
